a967f4fc77b82352ac97039ce8fc84bbf887764d
akitof11 <akitof11@gmail.com>
2026-08-23 14:34:34 +0900

    tauri

--- tauri/vite.config.js
+++ tauri/vite.config.js
@@ -29,4 +29,10 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+	resolve: {
+		alias: {
+			$lib: '/src/lib',
+			$comp:'/src/lib/components',
+		}
+	},
 }));
