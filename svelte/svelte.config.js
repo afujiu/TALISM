4ab836d45f2e9fa89f5d03f0b12fb8354991a807
akitof11 <akitof11@gmail.com>
2026-08-23 14:27:56 +0900

    a

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,7 +1,10 @@
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
+
 const config = {
 	preprocess: vitePreprocess(),
+
+
 	kit: {
 		adapter: adapter()
 	}
